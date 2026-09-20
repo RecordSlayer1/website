eb42c1baad8ff7926735e5a95eddce3b87c2eb87
RecordSlayer1 <patrijotas25@gmail.com>
2026-09-20 13:07:05 +0200

    copy static to public

--- src/main.py
+++ src/main.py
@@ -1,21 +1,33 @@
-from textnode import TextNode, TextType
-from htmlnode import HTMLNode
-from leafnode import LeafNode
-from parentnode import ParentNode
-from splitnodes import split_nodes_image, text_to_textnodes
-from markdown_blocks import block_type_to_html_node, BlockType, markdown_to_blocks
+import shutil
+import os
+
 
 def main()-> None:
-    md = '''
-1. item1
-2. item2
-3. item3
-'''
-    markdown = markdown_to_blocks(md)
-    node = block_type_to_html_node(markdown[0], BlockType.OLIST)
-    print(node.to_html())
-
-main()
+    root_path = './static'
+    copy_root_path = './public'
+    remove_and_copy_files(root_path, copy_root_path)
 
 
+def remove_and_copy_files(root_path: str, copy_root_path: str)-> None:
+    if os.path.exists(copy_root_path):
+        shutil.rmtree(copy_root_path)
+        print(f' - deleting folder "{copy_root_path}"...')
+    os.mkdir(copy_root_path)
+    print(f' - creating new folder "{copy_root_path}"...')
 
+    if not os.path.exists(root_path):
+        raise ValueError(f'root folder is missing "{root_path}"') 
+    
+    files = os.listdir(root_path)
+    for file in files:
+        target_file = os.path.join(root_path, file)
+        copy_path = os.path.join(copy_root_path, file)
+        if os.path.isfile(target_file):
+            shutil.copy(target_file, copy_path)
+            print(f' - making copy from "{target_file}" to "{copy_path}"...')
+        else:
+            remove_and_copy_files(target_file, copy_path)
+
+
+if __name__ == "__main__":
+    main()
